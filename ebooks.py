94384a6464641c9071569db385c674cbcf4787e9
Jona Abdinghoff <zeratax@firemail.cc>
2016-06-20 21:30:13 +0200

    Update ebooks.py

--- ebooks.py
+++ ebooks.py
@@ -14,6 +14,7 @@ print('------')
 async def on_ready():
     print('Discord: Logged in as')
     print(client.user.name)
+    
     print(client.user.id)
     print('------')
 
@@ -22,18 +23,38 @@ async def on_message(message):
     with open("log.txt", "a") as myfile:
         myfile.write(message.clean_content + " ")
         print(message.clean_content)
+    yesno = urllib.request.urlopen('https://pastebin.com/raw/90WCeZp9').read().decode('utf-8').split() # it's a file like object and works just like a file
 
     if client.user.mentioned_in(message):
-        with open("log.txt") as f:
-            text = f.read()
-            
-        text_model = markovify.Text(text)
-        shitpost = text_model.make_short_sentence(80)
-        if shitpost is not None:
-            shitpost = shitpost.format(message)
-            await client.send_message(message.channel, shitpost)  
+        if message.content.endswith('??'):
+            if 'or' in message.clean_content:
+                REMOVE_LIST = ["@tsumino_ebooks", "\?"]
+                remove = '|'.join(REMOVE_LIST)
+                regex = re.compile(r'('+remove+')', flags=re.IGNORECASE)
+                shitdecision = regex.sub("", message.clean_content).split("or")
+                shitdecision = random.choice (shitdecision).format(message)
+                await client.send_message(message.channel, shitdecision)
+                print(shitdecision)
+            else:
+                shitanswer = random.choice (yesno).format(message)
+                await client.send_message(message.channel, shitanswer)
+                print(shitanswer)
         else:
-            await client.send_message(message.channel, 'Fuck off~')
+            with open("log.txt") as f:
+                text = f.read()
+                
+            text_model = markovify.Text(text)
+            shitpost = text_model.make_short_sentence(50)
+            if shitpost is not None:
+                shitpost = shitpost.format(message)
+                await client.send_message(message.channel, shitpost)  
+            else:
+                shitpost = text_model.make_short_sentence(50)
+                if shitpost is not None:
+                    shitpost = shitpost.format(message)
+                    await client.send_message(message.channel, shitpost)
+                else:
+                    await client.send_message(message.channel, 'Fuck off~')
 
 def tweet():
 
@@ -42,7 +63,7 @@ def tweet():
 
     text_model = markovify.Text(text)
 
-    shittweet = text_model.make_short_sentence(80)
+    shittweet = text_model.make_short_sentence(50)
     print(shittweet)
     if shittweet is not None:
         if (len(shittweet) > 0 and len(shittweet) < 141):
